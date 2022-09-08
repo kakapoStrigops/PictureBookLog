017cfa09f38be3bc382d13dfd2fb51384c4cedff
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-08 12:38:53 +0000

    [update]会員情報の編集機能を実装

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -4,11 +4,25 @@ class Public::MembersController < ApplicationController
   end
 
   def edit
+    @member = Member.find(current_member.id)
   end
 
   def update
+    @member = Member.find(current_member.id)
+    if @member.update(member_params)
+      redirect_to member_path(@member.id)
+    else
+      render :edit
+    end
   end
 
   def index
   end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :name_kana, :nickname, :year_of_birth, :email)
+  end
+
 end
