437fde9c25f0847964cfb1f112b74a1cec63ab9a
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-10 01:37:31 +0000

    [update]楽天ブックス内での本の検索対象のジャンルを絵本に指定

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -4,7 +4,11 @@ class Public::BooksController < ApplicationController
 
   def index
     if params[:keyword]
-      @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+      @books = RakutenWebService::Books::Book.search({
+        title: params[:keyword],
+        booksGenreId: '001003',
+        hits: 20,
+        })
     end
   end
 end
