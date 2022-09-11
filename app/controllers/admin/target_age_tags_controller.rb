72333b0be5a423e02e41fb2d4bc70ee23fbdb152
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-11 07:07:41 +0000

    [add]admin_対象年齢タグの表示・作成・編集

--- app/controllers/admin/target_age_tags_controller.rb
+++ app/controllers/admin/target_age_tags_controller.rb
@@ -1,13 +1,31 @@
 class Admin::TargetAgeTagsController < ApplicationController
   def new
+    @target_age_tag = TargetAgeTag.new
   end
 
   def create
+    @target_age_tag = TargetAgeTag.new(target_age_tag_params)
+    @target_age_tag.save
+    redirect_to admin_tags_path
   end
 
   def edit
+    @target_age_tag = TargetAgeTag.find(params[:id])
   end
 
   def update
+    @target_age_tag = TargetAgeTag.find(params[:id])
+    if @target_age_tag.update(target_age_tag_params)
+      redirect_to admin_tags_path
+    else
+      render :edit
+    end
   end
+
+  private
+
+  def target_age_tag_params
+    params.require(:target_age_tag).permit(:target_age)
+  end
+
 end
