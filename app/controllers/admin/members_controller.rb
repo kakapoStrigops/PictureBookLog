f9c3ef001451bf023722d94e61bd259f6ef5956c
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-11 05:25:32 +0000

    [add]admin_会員編集ページの作成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -9,8 +9,22 @@ class Admin::MembersController < ApplicationController
   end
 
   def edit
+    @member = Member.find(params[:id])
   end
 
   def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to admin_member_path(@member.id)
+    else
+      render "edit"
+    end
   end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :name_kana, :nickname, :year_of_birth, :email, :self_introduction, :profile_image, :delete_status)
+  end
+
 end
