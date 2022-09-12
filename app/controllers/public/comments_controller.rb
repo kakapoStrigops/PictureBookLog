362dbc8ddcaaf944823bafb0887431272cbfb061
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-12 07:02:58 +0000

    [add]コメントの削除機能を追加

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -16,6 +16,13 @@ class Public::CommentsController < ApplicationController
   def update
   end
 
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to review_path(params[:review_id])
+  end
+
+  private
+
   def comment_params
     params.require(:comment).permit(:comment)
   end
