4b152a22d512977e9d28cc50bd110cf739493fdf
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-13 06:17:45 +0000

    [add]admin_レビューを一覧表示

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,13 +1,27 @@
 class Admin::ReviewsController < ApplicationController
   def index
+    @reviews = Review.all.page(params[:page]).per(10)
   end
 
   def show
+    @review = Review.find(params[:id])
+    @comments = Comment.where(review_id: @review.id).page(params[:page]).per(10)
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to admin_reviews_path
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:hidden_status)
+  end
+
 end
