461904d66976b540190272bfe251bd18ca387b27
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-13 13:34:37 +0000

    [update]admin_コメントの公開ステータスの編集機能を実装

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -5,7 +5,8 @@ class Admin::CommentsController < ApplicationController
   end
 
   def show
-    @comment = Comment.find(params[:comment_id])
+    # @comment = Comment.find_by(id: params[:id], review_id: params[:review_id])
+    @comment = Comment.find(params[:id])
   end
 
   def edit
@@ -13,7 +14,15 @@ class Admin::CommentsController < ApplicationController
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to admin_review_path(@comment.review_id)
+  end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:hidden_status)
   end
 
 end
