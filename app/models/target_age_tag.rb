9861f3462224a4c8bb13c696fe5e049243987a8b
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-09-14 10:23:23 +0000

    target_age_tagモデルの修正

--- app/models/target_age_tag.rb
+++ app/models/target_age_tag.rb
@@ -1,6 +1,6 @@
 class TargetAgeTag < ApplicationRecord
 
   has_many :review_target_age_tags, dependent: :destroy
-  has_many :target_age_tags, dependent: :review_target_age_tags
+  has_many :target_age_tags, through: :review_target_age_tags
 
 end
