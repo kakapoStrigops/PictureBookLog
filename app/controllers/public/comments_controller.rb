76cac19c854572cee23f86d9527a99263931a0d5
EC2 Default User <emi.yamada1109@gmail.com>
2022-11-17 13:08:17 +0000

    [update]コメント投稿機能の条件分岐を追加

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -7,8 +7,14 @@ class Public::CommentsController < ApplicationController
     comment = current_member.comments.new(comment_params)
     comment.review_id = review.id
     comment.member_id = current_member.id
-    comment.save
-    redirect_to review_path(review.id)
+    if comment.save
+      redirect_to review_path(review.id)
+    else
+      @review = Review.find_by(id: params[:review_id], hidden_status: false)
+      @comments = Comment.where(review_id: review.id, hidden_status: false)
+      @comment = Comment.new
+      render template: "public/reviews/show"
+    end
   end
 
   def destroy
